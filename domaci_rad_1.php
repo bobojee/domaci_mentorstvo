d93e5cae7afabb0697c4b942e558410a2be4f069
bobojee <166776789+bobojee@users.noreply.github.com>
2024-04-12 04:06:07 +0200

    Add files via upload

---
+++ domaci_rad_1.php
@@ -0,0 +1,21 @@
+<?php
+
+    $site_name = "Postani programer";
+    $a_tags = ["Glavna", "O nama", "Kontakt"];
+
+?>
+
+
+<html>
+        <head>
+            <title><?php echo $site_name; ?></title>
+        </head>
+        <body>
+            <h1>Postani programer</h1>
+            <nav>
+                <a href=""><?php echo $a_tags[0]; ?></a>
+                <a href=""><?php echo $a_tags[1]; ?></a>
+                <a href=""><?php echo $a_tags[2]; ?></a>
+            </nav>
+        </body>
+</html>
